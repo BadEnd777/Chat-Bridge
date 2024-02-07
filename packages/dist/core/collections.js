fc69d4cd0c3698aa669d97ff4ba7f91e486113dc
BadEnd777 <badend23@hotmail.com>
2024-02-07 14:48:39 +0700

    refactor(core): breaking change to collections class

--- packages/dist/core/collections.js
+++ packages/dist/core/collections.js
@@ -2,59 +2,30 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Collections = void 0;
 /**
- * Collections class.
- * @export
- * @class Collections
- * @template T
- */
+ * Collections class
+ * @class
+ * @classdesc A class to manage collections
+ * @property {Array} items - An array to store items
+ * @method add - A method to add an item to the collection
+ * @method get - A method to get an item from the collection
+ * @example
+ * const collections = new Collections();
+ *
+ * collections.add({ name: 'item1', value: 1 });
+ * collections.add({ name: 'item2', value: 2 });
+ * collections.get('item1'); // { name: 'item1', value: 1 }
+ * collections.get('item2'); // { name: 'item2', value: 2 }
+ * */
 var Collections = /** @class */ (function () {
     function Collections() {
-        this.items = {};
+        this.items = [];
     }
-    /**
-     * Add an item to the Collections.
-     * @param {T} item
-     */
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     Collections.prototype.add = function (item) {
-        var key = this.getKey(item);
-        this.items[key] = item;
+        this.items.push(item);
     };
-    /**
-     * Get an item from the Collections.
-     * @param {string} key
-     * @returns {T}
-     */
-    Collections.prototype.get = function (key) {
-        return this.items[key];
-    };
-    /**
-     * Get all items from the Collections.
-     * @returns {T[]}
-     */
-    Collections.prototype.getAll = function () {
-        return Object.values(this.items);
-    };
-    /**
-     * Remove an item from the Collections.
-     * @param {string} key
-     */
-    Collections.prototype.remove = function (key) {
-        delete this.items[key];
-    };
-    /**
-     * Remove all items from the Collections.
-     */
-    Collections.prototype.removeAll = function () {
-        this.items = {};
-    };
-    /**
-     * Get the key of an item.
-     * @private
-     * @param {T} item
-     * @returns {string}
-     */
-    Collections.prototype.getKey = function (item) {
-        return item.id;
+    Collections.prototype.get = function (name) {
+        return this.items.find(function (item) { return item.name === name; });
     };
     return Collections;
 }());
--- source/src/core/collections.ts
+++ source/src/core/collections.ts
@@ -1,62 +1,32 @@
-import { Identifiable } from '../interfaces';
-
 /**
- * Collections class.
- * @export
- * @class Collections
- * @template T
- */
-export class Collections<T extends Identifiable> {
-    private items: { [key: string]: T } = {};
+ * Collections class
+ * @class
+ * @classdesc A class to manage collections
+ * @property {Array} items - An array to store items
+ * @method add - A method to add an item to the collection
+ * @method get - A method to get an item from the collection
+ * @example
+ * const collections = new Collections();
+ *
+ * collections.add({ name: 'item1', value: 1 });
+ * collections.add({ name: 'item2', value: 2 });
+ * collections.get('item1'); // { name: 'item1', value: 1 }
+ * collections.get('item2'); // { name: 'item2', value: 2 }
+ * */
+export class Collections {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    private items: any[];
 
-    /**
-     * Add an item to the Collections.
-     * @param {T} item
-     */
-    public add(item: T) {
-        const key = this.getKey(item);
-        this.items[key] = item;
+    constructor() {
+        this.items = [];
     }
 
-    /**
-     * Get an item from the Collections.
-     * @param {string} key
-     * @returns {T}
-     */
-    public get(key: string): T {
-        return this.items[key];
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    add(item: any) {
+        this.items.push(item);
     }
 
-    /**
-     * Get all items from the Collections.
-     * @returns {T[]}
-     */
-    public getAll(): T[] {
-        return Object.values(this.items);
-    }
-
-    /**
-     * Remove an item from the Collections.
-     * @param {string} key
-     */
-    public remove(key: string) {
-        delete this.items[key];
-    }
-
-    /**
-     * Remove all items from the Collections.
-     */
-    public removeAll() {
-        this.items = {};
-    }
-
-    /**
-     * Get the key of an item.
-     * @private
-     * @param {T} item
-     * @returns {string}
-     */
-    private getKey(item: T): string {
-        return item.id;
+    get(name: string) {
+        return this.items.find((item) => item.name === name);
     }
 }
